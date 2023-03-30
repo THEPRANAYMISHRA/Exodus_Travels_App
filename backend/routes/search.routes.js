56bddb41fc5338f57a9bcea916c04ed339cfcd6e
Pranay <pmcanvas4501@gmail.com>
2023-03-30 21:59:32 +0530

    day-2

--- backend/routes/search.routes.js
+++ backend/routes/search.routes.js
@@ -2,10 +2,19 @@ const express=require("express")
 const searchRouter=express.Router()
 const SearchModal=require("../model/serach.model")
 
-searchRouter.get("/available",async(req,res)=>{
-    let {from,to,checkIn,checkOut,Travellers}=req.body
-    
-    
+searchRouter.post("/available",async(req,res)=>{
+    let {Booking_type,from,to,checkIn,checkOut,seats}=req.body
+    try {
+        let data= await SearchModal.find({
+            Booking_type:Booking_type,
+            checkIn:{$gte:checkIn}, 
+            checkOut:{$lte:checkOut},
+            seats:{$gt:0}
+        })
+        res.send(data)
+    } catch (error) {
+        res.status(400).send({"msg":"no service available!"})
+    }
 })
 
 module.exports=searchRouter
