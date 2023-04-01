3d68e7ab0acd9a5310bd3435f40c1967a423a9e7
Pranay <pmcanvas4501@gmail.com>
2023-04-01 23:40:55 +0530

    day-4

--- backend/routes/booking.routes.js
+++ backend/routes/booking.routes.js
@@ -1,7 +1,24 @@
 const express=require("express")
 const bookingRouter=express.Router()
+const UserModal=require("../model/user.model")
+const auth=require("../middleware/notes.middleware")
+const BoookingModel=require("../model/booking.model")
 
-
-bookingRouter.post("/book",async(req,res)=>{
-    
+bookingRouter.get("/history",auth,async(req,res)=>{
+    let {userId}=req.body
+    let historydata= await BoookingModel.find({userId})
+    res.send(historydata)
 })
+
+bookingRouter.post("/booking",auth,async(req,res)=>{
+    try {
+        const allBooking=new BoookingModel(req.body)
+        await allBooking.save()
+        res.status(200).send({"msg":"boooking successful!"})
+    } catch (error) {
+        res.status(400).send({"msg":"boooking failed!"})
+    }
+})
+
+
+module.exports=bookingRouter
